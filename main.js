9c74ecb070a04ffb90c2297f406d472521f4c5e3
FrankYous <f_s_yousefi@yahoo.com>
2023-04-27 16:14:29 +0330

    Add CLI input counting functionality

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+function main(){
+    inputCount = process.argv.length -2
+    try{
+        if (inputCount < 1){
+            throw new Error('No input argument was entered.')
+        } else if (inputCount > 1){
+            throw new Error('More than one input argument was entered.')
+        } else {
+            console.log(`Initializing the crawler at ${process.argv[2]}...`)
+        }
+    }
+    catch(err){
+        console.log(err.message)
+    }
+}
+
+
+main()
