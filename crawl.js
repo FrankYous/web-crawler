f7aaf9b7b444a208c0d8dd10fd6ae9325faf6df8
FrankYous <f_s_yousefi@yahoo.com>
2023-04-27 12:59:18 +0330

    Add tests for extracting URLs from HTML

--- crawl.js
+++ crawl.js
@@ -1,13 +1,23 @@
 function normalizeURL (inputURL){
     const myURL = new URL(inputURL)
-    const myHostname = myURL.hostname.toLowerCase()
-    let myPathname = myURL.pathname.toLowerCase()
+    const myHostname = myURL.hostname
+    let myPathname = myURL.pathname
     if (myPathname.at(-1) === '/'){
         myPathname = myPathname.slice(0,-1)
     }
     return (`${myHostname}${myPathname}`)
 }
 
+function getURLsFromHTML (htmlBody, baseURL){
+    const { JSDOM } = require('jsdom')
+    const dom = new JSDOM(htmlBody)
+    let links = dom.window.document.querySelectorAll('a')
+    for (let link of links){
+        let linkURL = new URL(link)
+    }
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,23 +1,66 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const { getURLsFromHTML } = require('./crawl.js')
 
 
-test('removes the protocol', () => {
-    expect(normalizeURL('http://www.gnu.org')).toBe('www.gnu.org');
-});
+describe('normalizing the URL', () => {
+    test('removes the protocol', () => {
+        expect(normalizeURL('http://www.gnu.org')).toBe('www.gnu.org');
+    });
 
-test('lowercases all letters', () => {
-    expect(normalizeURL('https://www.GNU.org')).toBe('www.gnu.org');
-});
+    test('lowercases all letters', () => {
+        expect(normalizeURL('https://www.GNU.org')).toBe('www.gnu.org');
+    });
 
-test('removes the slash at the end of hostname', () => {
-    expect(normalizeURL('https://www.gnu.org/')).toBe('www.gnu.org');
-});
+    test('removes the slash at the end of hostname', () => {
+        expect(normalizeURL('https://www.gnu.org/')).toBe('www.gnu.org');
+    });
 
-test('removes the slash at the end of pathname', () => {
-    expect(normalizeURL('https://www.gnu.org/foo/bar/')).toBe('www.gnu.org/foo/bar');
-});
+    test('removes the slash at the end of pathname', () => {
+        expect(normalizeURL('https://www.gnu.org/foo/bar/')).toBe('www.gnu.org/foo/bar');
+    });
 
-test('all cases combined', () => {
-    expect(normalizeURL('http://www.gNu.Org/foo/bar/')).toBe('www.gnu.org/foo/bar');
-});
+    test('URL normalization - all cases combined', () => {
+        expect(normalizeURL('http://www.gNu.Org/foo/bar/')).toBe('www.gnu.org/foo/bar');
+    });
+})
+
+describe('extracting URLs from HTML', () => {
+    test('changes relative URL to absolute url', () => {
+        expect(getURLsFromHTML(
+            `<div class="btn-left">
+            <a href="/distros/free-distros.html">Try GNU/Linux</a></div>
+            <div style="clear:both"></div>`,
+            'https://www.gnu.org'
+        )).toBe('https://www.gnu.org/distros/free-distros.html');
+    })
+
+    test('returns external URLs unchanged', () => {
+        expect(getURLsFromHTML(
+            `<div class="btn-right">... or <a
+            href="https://directory.fsf.org/wiki/Free_Software_Directory:Free_software_replacements">
+            Try parts of GNU</a></div>`,
+            'https://www.gnu.org'
+        )).toBe('https://directory.fsf.org/wiki/Free_Software_Directory:Free_software_replacements')
+    })
+
+    test('extracts URLs from HTML - combined case', () => {
+        expect(getURLsFromHTML(
+            `<div class="btn-right">... or <a
+            href="https://directory.fsf.org/wiki/Free_Software_Directory:Free_software_replacements">
+            Try parts of GNU</a></div>
+            <div style="clear:both"></div>
+            
+            <h2>What is the Free Software Movement?</h2>
+            
+            <p>The free software movement campaigns to win for the users of
+            computing the freedom that comes from free software.  Free software
+            puts its users in control of their own computing.  Nonfree software
+            puts its users under the power of the software's developer.  See
+            <a href="/audio-video/philosophy-recordings.html#rms-201404070">
+            the video explanation</a>.</p>`,
+            'https://www.gnu.org'
+        )).toBe(['https://directory.fsf.org/wiki/Free_Software_Directory:Free_software_replacements',
+        'https://www.gnu.org/audio-video/philosophy-recordings.html#rms-201404070'])
+    })
+})
