3d1545b7a5381235fc1fc93a01e740f654689746
FrankYous <f_s_yousefi@yahoo.com>
2023-04-29 17:04:21 +0330

    Add the sorting function

--- sort.js
+++ sort.js
@@ -1,5 +1,7 @@
 function sortDict(dictionary){
-    
+    const unsortedArray = Object.entries(dictionary)
+    const sortedArray = unsortedArray.sort(function (a,b) {return b[1]-a[1]})
+    return sortedArray
 }
 
 module.exports={
