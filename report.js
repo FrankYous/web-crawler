bee477ac9f7392eaac9328dbed8fe0d49edc26c1
FrankYous <f_s_yousefi@yahoo.com>
2023-04-29 16:26:10 +0330

    Add report function

--- report.js
+++ report.js
@@ -1,8 +1,15 @@
-function printReport(pages){
-    console.log('=====Report is starting...=====')
-    
-}
+const { sortDict } = require('./sort')
 
-module.exports {
+function printReport(pages){
+    console.log('=====Report of the Crawl=====')
+    const sortedPages = sortDict(pages)
+    for (let sortedPage of sortedPages){
+        const url = sortedPage[0]
+        const count =  sortedPage[1]
+        console.log(`Found ${count} internal links to ${url}}`)
+    }
+    console.log('======End of the Report======')
+}
+module.exports = {
     printReport
 }
--- sort.js
+++ sort.js
@@ -1,3 +1,7 @@
 function sortDict(dictionary){
     
 }
+
+module.exports={
+    sortDict
+}
